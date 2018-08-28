d6b63c19af7bceefb8b017bb4d1772862609c81e
Ilya Bodrov <bodrovia@yamoney.ru>
2018-08-28 12:57:20 +0300

    Исправлены баги при подтверждении платежда и отмене.

--- wa-apps/shop/plugins/yamodule_api/lib/config/plugin.php
+++ wa-apps/shop/plugins/yamodule_api/lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name' => 'Y.CMS Shop-Script 2',
     'description' => 'Набор модулей Яндекс (Яндекс.Деньги, Яндекс.Маркет, Яндекс.Метрика)',
     'vendor' => '98765',
-    'version' => '1.0.16',
+    'version' => '1.0.17',
     'img' => '/img/logo.png',
     'frontend' => true,
     'shop_settings' => true,
